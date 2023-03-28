03309352f6929ae1373a10c82f20c4824079424e
Daniel Asante <baremetals16@gmail.com>
2023-03-28 19:38:33 +0100

    deleted controllers

--- controllers/posts/getPost.js
+++
@@ -1,7 +0,0 @@
-const BlogPost = require('../../models/BlogPost.js');
-
-module.exports = async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id);
-  // console.log(req.params.id);
-  res.render('post', { blogpost });
-};
